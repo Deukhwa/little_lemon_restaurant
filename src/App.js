9e93a730eebaa0863e49512d5851476d6a37d35a
Deukhwa Jeong <danjeong@deukhwas-mbp-2.home>
2023-06-02 11:49:13 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useState } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, Container } from '@chakra-ui/react';
 
-import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import './App.css';
+import HeroSection from './components/HeroSection';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider>
-      <Routes>
-        <Route path="/" element={<Nav />} />
-        //...
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Box textAlign="center" py={8}>
+        <Container maxW="container.lg">
+          <Nav />
+          <HeroSection />
+          <Highlights />
+          <Testimonials />
+          <About />
+          <Footer />
+        </Container>
+      </Box>
     </ChakraProvider>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Box, HStack, Image } from '@chakra-ui/react';
 
-const Nav = () => {
+const NavigationBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box py={4} bg="gray.200">
+      <HStack
+        spacing={4}
+        alignItems="center"
+        justifyContent="space-between"
+        px={8}
+      >
+        <Box w="8" h="8" bg="gray.500" transform="rotate(-45deg)" />
+        <HStack spacing={8}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/menu">Menu</a>
+          <a href="/reservations">Reservations</a>
+          <a href="/order">Order Online</a>
+          <a href="/login">Login</a>
+        </HStack>
+      </HStack>
+    </Box>
   );
 };
 
-export default Nav;
+export default NavigationBar;
